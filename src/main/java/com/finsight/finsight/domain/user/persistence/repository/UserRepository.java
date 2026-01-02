47ba313c03092490c32456c3a08e0a00bb292c6e
Namyeon Kim <ouroboros0519@gmail.com>
2026-01-03 04:15:43 +0900

    #14 ✨ feat: UserRepository, UserAuthRepository 구현

---
+++ src/main/java/com/finsight/finsight/domain/user/persistence/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.finsight.finsight.domain.user.persistence.repository;
+
+import com.finsight.finsight.domain.user.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // 닉네임 중복 확인
+    boolean existsByNickname(String nickname);
+}
