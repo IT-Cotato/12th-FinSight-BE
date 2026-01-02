984bd0c676edf37576afe75e5e6cf899b40b1654
Namyeon Kim <ouroboros0519@gmail.com>
2026-01-03 04:18:28 +0900

    #14 ✨ feat: 인증 관련 ErrorCode 추가

--- src/main/java/com/finsight/finsight/global/exception/ErrorCode.java
+++ src/main/java/com/finsight/finsight/global/exception/ErrorCode.java
@@ -14,6 +14,21 @@ public enum ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다.", "COMMON-002"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다.", "COMMON-003"),
 
+    //400 - auth
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.", "AUTH400_1"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.", "AUTH400_2"),
+    VERIFICATION_CODE_MISMATCH(HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다.", "AUTH400_3"),
+    VERIFICATION_CODE_EXPIRED(HttpStatus.BAD_REQUEST, "인증번호가 만료되었습니다.", "AUTH400_4"),
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다.", "AUTH400_5"),
+
+    //401 - Auth
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다.", "AUTH401_1"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.", "AUTH401_2"),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다.", "AUTH401_3"),
+
+    //404
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "AUTH404_1"),
+
     //500
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.", "COMMON-004"),
     ;
