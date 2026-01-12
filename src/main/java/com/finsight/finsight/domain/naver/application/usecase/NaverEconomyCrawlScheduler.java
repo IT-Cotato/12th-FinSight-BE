15a46478a83ff36189d1590d885a9bcb3748a59a
김세현 <kkshyun56@gmail.com>
2026-01-12 11:15:56 +0900

    #24 :recycle: refactor: 크롤링 및 AI 작업 자동 설정 수정

--- src/main/java/com/finsight/finsight/domain/naver/application/usecase/NaverEconomyCrawlScheduler.java
+++ src/main/java/com/finsight/finsight/domain/naver/application/usecase/NaverEconomyCrawlScheduler.java
@@ -3,6 +3,7 @@ package com.finsight.finsight.domain.naver.application.usecase;
 import com.finsight.finsight.domain.naver.domain.service.NaverCrawlerService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@ConditionalOnProperty(value = "naver.crawler.enabled", havingValue = "true", matchIfMissing = true)
 public class NaverEconomyCrawlScheduler {
 
     private final NaverCrawlerService crawlerService;
