12e82bbf0ef6cc127fe978ad76301b08daa27bc0
Namyeon Kim <ouroboros0519@gmail.com>
2026-01-15 07:01:18 +0900

    #31 ♻️ refactor: Auth request dto 수정

--- src/main/java/com/finsight/finsight/domain/auth/application/dto/request/KakaoSignupRequest.java
+++ src/main/java/com/finsight/finsight/domain/auth/application/dto/request/KakaoSignupRequest.java
@@ -8,6 +8,5 @@ public record KakaoSignupRequest(
         String kakaoId,
 
         @NotBlank(message = "닉네임은 필수입니다.")
-        @Size(min = 1, max = 10, message = "닉네임은 1-10자여야 합니다.")
         String nickname
 ) {}
