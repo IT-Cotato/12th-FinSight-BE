5995a3baa14b7cfb9dbc9bd4c9de734f8e5ad5c8
김민규 <alsrb0332@naver.com>
2026-01-16 01:32:41 +0900

    #25 :sparkles: feat: 인기순 구현(redis + 5분마다 db 저장)

---
+++ src/main/java/com/finsight/finsight/global/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.finsight.finsight.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Key는 String으로 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value는 기본적으로 String으로 처리 (필요시 Jackson 등 사용 가능)
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+}
